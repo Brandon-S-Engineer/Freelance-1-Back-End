b1c1d716c3b1066338dc04294200875975c1531b
Brandon Soria <brandon.se.25@gmail.com>
2025-05-28 13:08:00 -0600

    u

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -1,3 +1,9 @@
+const BillboardPage = async ({ params }: { params: { billboardId: string } }) => {
+  return <div>This is a form for billboards</div>;
+};
+
+export default BillboardPage;
+
 // const page = () => {
 //   return <div>page</div>;
 // };
